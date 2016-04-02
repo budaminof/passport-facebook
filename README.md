1fd1249ed5450b7aaa7de36bf9985d1b21741862
bud aminof <budaminof@gmail.com>
2016-04-02 14:10:03 -0600

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+##############
+learning to use OAuth facebook.
+
+
+// to change url in FB developers
+ https://developers.facebook.com/apps/521586178025497/settings/basic/
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var unirest = require('unirest');
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log('==============session',req.session.passport);
